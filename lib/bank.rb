46998051f7ea4c38375679b45f1cabed78db3a45
milesillsley <milesillsley@hotmail.co.uk>
2017-06-12 11:42:00 +0100

    remove attr_reader for balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,10 @@
 class Bank
-  attr_reader :balance
+  #attr_reader :balance
   def initialize
     @balance = 0
+  end
+  def balance
+    @balance
   end
   def deposit(amount)
     @balance =+ amount
